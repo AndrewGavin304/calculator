18b40c3717001eb08b18fd98f558f751797bce0b
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-11 11:24:32 -0400

    Add basic button functionality

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ function divide(a, b) {return a/b}
 
 function operate(func, a, b) {return func(a, b)}
 
+function clear() {
+  display.textContent = "0"
+  return;
+}
+
+const buttons = document.querySelectorAll('.button')
+const display = document.getElementById("display")
+
+buttons.forEach(function(button){
+  button.addEventListener('click', event => {
+    if (event.target.classList.contains("operator")) {
+      console.log("fff")
+    }
+    if (event.target.id == "clear") {
+      clear()
+    }
+    else if (display.textContent == 0) {
+      display.textContent = button.textContent
+    }
+    else {
+      display.textContent += button.textContent
+    }
+  }
+)})
